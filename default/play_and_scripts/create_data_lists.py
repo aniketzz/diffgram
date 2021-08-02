a72e5a44b725b7a07f449b96f11a2b18761f12b5
Pablo <pjestradac@gmail.com>
2021-08-02 16:32:09 -0600

    add tutorial files

---
+++ default/play_and_scripts/create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/home/pablo/Dropbox/diffgram/pytorch_obj_tutorial/VOCdevkit/VOC2007',
+                      voc12_path='/home/pablo/Dropbox/diffgram/pytorch_obj_tutorial/VOCdevkit/VOC2012',
+                      output_folder='./')

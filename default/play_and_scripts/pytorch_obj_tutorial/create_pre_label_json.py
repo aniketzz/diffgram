654bd405b894a16a2d47014c0bf30f3fdba2720a
Pablo <pjestradac@gmail.com>
2021-08-02 15:37:07 -0600

    generate json data script

--- default/play_and_scripts/pytorch_obj_tutorial/create_pre_label_json.py
+++ default/play_and_scripts/pytorch_obj_tutorial/create_pre_label_json.py
@@ -1,11 +1,33 @@
 import os
 import xml.etree.ElementTree as ET
-
+import json
 result = []
-
-for filename in os.listdir('VOCdevkit/VOC2012/Annotations'):
-
+directory = './../../../pytorch_obj_tutorial/VOCdevkit/VOC2012/Annotations'
 
 
-tree = ET.parse('country_data.xml')
-root = tree.getroot()
+def etree_to_dict(t):
+    if type(t) is ET.ElementTree: return etree_to_dict(t.getroot())
+    return {
+        **t.attrib,
+        'text': t.text,
+        **{e.tag: etree_to_dict(e) for e in t}
+    }
+
+for dirpath, _, filenames in os.walk(directory):
+    for f in filenames:
+        final_path = os.path.abspath(os.path.join(dirpath, f))
+        tree = ET.parse(final_path)
+        root = tree.getroot()
+        dict_data = etree_to_dict(root)
+        result.append({
+            'filename': dict_data['filename']['text'],
+            'type': 'box',
+            'class': dict_data['object']['name']['text'],
+            'x_min': dict_data['object']['bndbox']['xmin']['text'],
+            'x_max': dict_data['object']['bndbox']['xmax']['text'],
+            'y_min': dict_data['object']['bndbox']['ymin']['text'],
+            'y_max': dict_data['object']['bndbox']['ymax']['text']
+        })
+
+with open('VOC_prelabels.json', 'w') as outfile:
+    json.dump(result, outfile)
